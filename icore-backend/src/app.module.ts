96d9d14a94b35c282addb946267225bfd1c14864
Janitha Senani Karunarathna <janithakarunarathna12@gmail.com>
2025-06-06 22:48:47 +0530

    implemented crud for user

--- icore-backend/src/app.module.ts
+++ icore-backend/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './config/database/database.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    })
+    }),
+    DatabaseModule,
+    UserModule
   ],
   controllers: [],
   providers: [],
