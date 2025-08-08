0ec18624bff573d939c61445ed4da07c21a3fedb
WebCooper <senani.online@gmail.com>
2025-08-08 10:31:19 +0530

    modify axios instance to use env

--- frontend-public/lib/axios/axiosInstance.ts
+++ frontend-public/lib/axios/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000';
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000', // your NestJS backend URL
+  baseURL, 
   headers: {
     'Content-Type': 'application/json',
   },
