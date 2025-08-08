3d1fae3c47809ffad4334e908e6dd0f4c88dcbf4
WebCooper <senani.online@gmail.com>
2025-08-08 10:24:07 +0530

    add dockerfiles for frontend and backend

--- icore-backend/src/main.ts
+++ icore-backend/src/main.ts
@@ -21,13 +21,13 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const corsOptions: CorsOptions = {
-    origin: ['http://localhost:3001', 'https://theicore.org'], // Allow only your Next.js app to access the API
+    origin: ['http://localhost:3001', 'https://theicore.org', 'https://dev-portal.theicore.org'], // Allow only your Next.js app to access the API
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow cookies to be sent with requests
     optionsSuccessStatus: 204, // Some legacy browsers choke on 204
   };
 
   app.enableCors(corsOptions);
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
