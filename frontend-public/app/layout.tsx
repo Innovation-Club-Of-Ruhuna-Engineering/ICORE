abd013e0808fa1b699d778a476fa418872bc3769
WebCooper <senani.online@gmail.com>
2025-08-09 18:13:19 +0530

    modify the layout.tsx

--- frontend-public/app/layout.tsx
+++ frontend-public/app/layout.tsx
@@ -1,103 +1,103 @@
 import type { Metadata } from "next";
-import localFont from 'next/font/local'
+// import localFont from 'next/font/local'
 import "./globals.css";
 import { AuthProvider } from "@/contexts/userAuthContext";
 import { Toaster } from 'react-hot-toast';
 
-const inter = localFont({
-  src: [
-    {
-      path: '../public/fonts/Inter_28pt-Thin.ttf',
-      weight: '100',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-ThinItalic.ttf',
-      weight: '100',
-      style: 'italic',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-ExtraLight.ttf',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-ExtraLightItalic.ttf',
-      weight: '200',
-      style: 'italic',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-Light.ttf',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-LightItalic.ttf',
-      weight: '300',
-      style: 'italic',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {path: '../public/fonts/Inter_28pt-Italic.ttf',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-Medium.ttf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-MediumItalic.ttf',
-      weight: '500',
-      style: 'italic',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-SemiBold.ttf',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-SemiBoldItalic.ttf',
-      weight: '600',
-      style: 'italic',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-BoldItalic.ttf',
-      weight: '700',
-      style: 'italic',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-ExtraBold.ttf',
-      weight: '800',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-ExtraBoldItalic.ttf',
-      weight: '800',
-      style: 'italic',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-Black.ttf',
-      weight: '900',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Inter_28pt-BlackItalic.ttf',
-      weight: '900',
-      style: 'italic',
-    }
-    
-  ]
-})
+// Temporarily commenting out font configuration
+// const inter = localFont({
+//   src: [
+//     {
+//       path: '../public/fonts/Inter_28pt-Thin.ttf',
+//       weight: '100',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-ThinItalic.ttf',
+//       weight: '100',
+//       style: 'italic',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-ExtraLight.ttf',
+//       weight: '200',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-ExtraLightItalic.ttf',
+//       weight: '200',
+//       style: 'italic',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-Light.ttf',
+//       weight: '300',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-LightItalic.ttf',
+//       weight: '300',
+//       style: 'italic',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-Regular.ttf',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//     {path: '../public/fonts/Inter_28pt-Italic.ttf',
+//       weight: '400',
+//       style: 'italic',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-Medium.ttf',
+//       weight: '500',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-MediumItalic.ttf',
+//       weight: '500',
+//       style: 'italic',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-SemiBold.ttf',
+//       weight: '600',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-SemiBoldItalic.ttf',
+//       weight: '600',
+//       style: 'italic',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-Bold.ttf',
+//       weight: '700',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-BoldItalic.ttf',
+//       weight: '700',
+//       style: 'italic',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-ExtraBold.ttf',
+//       weight: '800',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-ExtraBoldItalic.ttf',
+//       weight: '800',
+//       style: 'italic',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-Black.ttf',
+//       weight: '900',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../public/fonts/Inter_28pt-BlackItalic.ttf',
+//       weight: '900',
+//       style: 'italic',
+//     }
+//   ]
+// })
 
 export const metadata: Metadata = {
   title: "the icore",
@@ -111,7 +111,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en">
       <body>
         <AuthProvider>
           <Toaster position="top-right" />
