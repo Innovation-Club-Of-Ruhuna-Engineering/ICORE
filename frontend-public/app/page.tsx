af4e2ea4af2ce49b50794bdc050c16b594431775
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2025-08-16 14:04:15 +0530

    homepage basic draft

--- frontend-public/app/page.tsx
+++ frontend-public/app/page.tsx
@@ -1,25 +1,24 @@
-
-import Hero from "@/components/home/hero";
-import { Footer } from "@/components/shared/footer";
-import { AboutSection } from "@/components/home/about-section";
-import { FeaturedProjects } from "@/components/home/featured-projects";
-import { HowToJoin } from "@/components/home/how-to-join";
-import { TechStack } from "@/components/home/tech-stack";
-import { Testimonials } from "@/components/home/testimonials";
-import { NewsletterCTA } from "@/components/home/newsletter-cts";
+import Navbar from "@/components/navbar"
+import Hero from "@/components/home/hero"
+import Features from "@/components/home/features"
+import Footer from "@/components/home/footer"
 
 export default function Home() {
   return (
-    <div >
-       
-        <Hero />   
-        <AboutSection />
-        <FeaturedProjects />
-        <HowToJoin />
-        <TechStack />
-        <Testimonials />
-        <NewsletterCTA />
+    <div className="relative min-h-screen">
+      {/* Background gradients */}
+      <div className="pointer-events-none fixed inset-0">
+        <div className="absolute inset-0 bg-gradient-to-b from-background via-background/90 to-background" />
+        <div className="absolute right-0 top-0 h-[500px] w-[500px] bg-blue-500/10 blur-[100px]" />
+        <div className="absolute bottom-0 left-0 h-[500px] w-[500px] bg-purple-500/10 blur-[100px]" />
+      </div>
+
+      <div className="relative z-10">
+        <Navbar />
+        <Hero />
+        <Features />
         <Footer />
+      </div>
     </div>
-  );
+  )
 }
