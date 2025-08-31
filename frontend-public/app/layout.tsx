a34a290f7377e8a9374e6e8dd81d88fa694d7cd9
WebCooper <senani.online@gmail.com>
2025-08-31 23:04:55 +0530

    resolve build errors and merge errors

--- frontend-public/app/layout.tsx
+++ frontend-public/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar";
-import { Footer } from "@/components/shared/footer";
 import { AuthProvider } from "@/contexts/userAuthContext";
 import { Toaster } from "react-hot-toast";
 
@@ -31,9 +29,9 @@ export default function RootLayout({
       <body className={`${outfit.variable} bg-background text-foreground antialiased`}>
         <AuthProvider>
           <Toaster position="top-right" />
-          <Navbar />
+  
           {children}
-          <Footer />
+         
         </AuthProvider>
       </body>
     </html>
